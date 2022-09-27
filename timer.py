9a48fde175bdeeace33d00156c4335e2e3b9c3b9
DESKTOP-JQAIFQV\User <carlospereira.ti@gmail.com>
2022-09-26 22:46:06 -0300

    Removed reset button \ added alert msg in 30 min

--- timer.py
+++ timer.py
@@ -10,7 +10,7 @@ root.geometry('400x220')
 root.resizable(0,0)
 photo = PhotoImage(file="clock.png")
 root.iconphoto(True, photo)
-root.title('Timer')
+root.title(' My Break')
 
 
 # variables
@@ -56,9 +56,12 @@ def update():
     if seconds == 60:
         minutes += 1
         seconds = 0
-    if minutes == 60:
-        hours += 1
-        minutes = 0
+    if minutes == 30 and seconds == 1:
+        # hours += 1
+        # minutes = 0
+        pause()
+        tk.messagebox.showwarning(title="My Break", message="Alert: Your break has finished.")
+
     # format time 
     hours_string = f'{hours}' if hours > 9 else f'0{hours}'
     minutes_string = f'{minutes}' if minutes > 9 else f'0{minutes}'
@@ -79,9 +82,9 @@ start_button = tk.Button(text='Start', height=2, width=7, font=('Calibri', 15), 
 start_button.pack(side=tk.LEFT, padx=10, pady=10)
 pause_button = tk.Button(text='Pause', height=2, width=7, font=('Calibri', 15), command=pause)
 pause_button.pack(side=tk.LEFT, padx=10, pady=10)
-reset_button = tk.Button(text='Reset', height=2, width=7, font=('Calibri', 15), command=reset)
-reset_button.pack(side=tk.LEFT, padx=10, pady=10)
+# reset_button = tk.Button(text='Reset', height=2, width=7, font=('Calibri', 15), command=reset)
+# reset_button.pack(side=tk.LEFT, padx=10, pady=10)
 close_button = tk.Button(text='Close', height=2, width=7, font=('Calibri', 15), command=root.quit)
-close_button.pack(side=tk.LEFT, padx=10, pady=10)
+close_button.pack(side=tk.RIGHT, padx=10, pady=10)
 
 root.mainloop()
